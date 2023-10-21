9026ac39f29e8345c623e48634e8bda8c4f1dcd8
dlobankov <lobankov082000@gmail.com>
2023-10-21 18:57:31 +0300

    fix

--- bot.py
+++ bot.py
@@ -3,8 +3,12 @@ import regex
 from threading import Thread
 import whisper
 from pytube import YouTube
+import time
 
 youtybe_reg = "https:\/\/www\.youtube\.com\/watch\?v="
+youtybe_location = "/temp"
+
+max_length=100
 
 bot = telebot.TeleBot('6487666443:AAFUZhjk4nbrRMaI6G8D5_BiyXqHnoW-wqU')
 
@@ -21,13 +25,29 @@ def polling_bot():
     bot.polling()
 
 def analyse_text(message:telebot.types.Message):
+    start_time = time.localtime()
     yt = YouTube(message.text)
-    fn = yt.streams.filter(type="audio")[0].download()
+    fn = yt.streams.filter(type="audio")[0].download(youtybe_location)
 
-    model = whisper.load_model("small")
+    model = whisper.load_model("tiny")
     res = model.transcribe(fn)
 
-    bot.send_message(message.chat.id, res["text"])
+    text = res["text"]
+
+    parts = []
+    if len(text) <= max_length:
+        parts.append(text)
+    else:
+        for i in range(0, len(text), max_length):
+            parts.append(text[i:i + max_length])
+            print(text[i:i + max_length], '\n\n')
+
+    for i in parts:  
+        bot.send_message(message.chat.id, res["text"])
+
+    stop_time = time.localtime()
+
+    print(f'Data about: {stop_time-start_time}')  
 
 if __name__ == "__main__":
     Thread(target=polling_bot).start()
